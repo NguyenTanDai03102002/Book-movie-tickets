a786764e898c940ee6f710e4b9bd019e17ec4f42
NguyenTanDai03102002 <daib2014647@student.ctu.edu.vn>
2024-06-07 10:01:43 +0700

    BE

--- src/main/java/vnpt/movie_booking_be/service/ScreeningService.java
+++ src/main/java/vnpt/movie_booking_be/service/ScreeningService.java
@@ -1,5 +1,6 @@
 package vnpt.movie_booking_be.service;
 
+import vnpt.movie_booking_be.dto.request.ScreeningCreationRequest;
 import vnpt.movie_booking_be.dto.response.ScreeningResponse;
 
 import java.util.List;
@@ -8,4 +9,10 @@ public interface ScreeningService {
     List<ScreeningResponse> getScreeningByCityAndMovie(String city, int movieId);
 
     List<ScreeningResponse> getScreeningByCinema(int cinemaId);
+
+    void createScreening(ScreeningCreationRequest request);
+
+    void updateScreening(int screeningId, ScreeningCreationRequest request);
+
+    void deleteScreening(int screeningId);
 }
--- src/main/java/vnpt/movie_booking_be/service/ScreeningServiceImpl.java
+++ src/main/java/vnpt/movie_booking_be/service/ScreeningServiceImpl.java
@@ -2,9 +2,14 @@ package vnpt.movie_booking_be.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vnpt.movie_booking_be.dto.request.ScreeningCreationRequest;
 import vnpt.movie_booking_be.dto.response.ScreeningResponse;
 import vnpt.movie_booking_be.mapper.ScreeningMapper;
+import vnpt.movie_booking_be.models.Auditorium;
+import vnpt.movie_booking_be.models.Movie;
 import vnpt.movie_booking_be.models.Screening;
+import vnpt.movie_booking_be.repository.AuditoriumRepository;
+import vnpt.movie_booking_be.repository.MovieRepository;
 import vnpt.movie_booking_be.repository.ScreeningRepository;
 
 import java.util.List;
@@ -15,6 +20,12 @@ public class ScreeningServiceImpl implements ScreeningService{
 
     @Autowired
     private ScreeningRepository screeningRepository;
+
+    @Autowired
+    private AuditoriumRepository auditoriumRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
 
     @Autowired
     private ScreeningMapper screeningMapper;
@@ -33,5 +44,34 @@ public class ScreeningServiceImpl implements ScreeningService{
                 .collect(Collectors.toList());
     }
 
+    @Override
+    public void createScreening(ScreeningCreationRequest request) {
+        Auditorium auditorium = auditoriumRepository.findById(request.getAuditoriumId()).orElseThrow(()->new RuntimeException("Auditorium not found"));
+        Movie movie = movieRepository.findById(request.getMovieId()).orElseThrow(()->new RuntimeException("Movie not found"));
+        screeningRepository.save(Screening.builder()
+                        .date(request.getDate())
+                        .start(request.getStart())
+                        .auditorium(auditorium)
+                        .movie(movie)
+                .build());
+    }
+
+    @Override
+    public void updateScreening(int screeningId, ScreeningCreationRequest request) {
+        Screening screening = screeningRepository.findById(screeningId).orElseThrow(()->new RuntimeException("Screening not found"));
+        Auditorium auditorium = auditoriumRepository.findById(request.getAuditoriumId()).orElseThrow(()->new RuntimeException("Auditorium not found"));
+        Movie movie = movieRepository.findById(request.getMovieId()).orElseThrow(()->new RuntimeException("Movie not found"));
+        screening.setStart(request.getStart());
+        screening.setDate(request.getDate());
+        screening.setAuditorium(auditorium);
+        screening.setMovie(movie);
+        screeningRepository.save(screening);
+    }
+
+    @Override
+    public void deleteScreening(int screeningId) {
+        Screening screening = screeningRepository.findById(screeningId).orElseThrow(()->new RuntimeException("Screening not found"));
+        screeningRepository.delete(screening);
+    }
 
 }
