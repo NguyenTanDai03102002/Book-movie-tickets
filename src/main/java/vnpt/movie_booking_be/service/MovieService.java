dd2ce82f835321dc16de33073bc0cafb9c45b445
cognuhn <congminhn855@gmail.com>
2024-06-28 14:33:32 +0700

    getmovie da xoa

--- src/main/java/vnpt/movie_booking_be/service/MovieService.java
+++ src/main/java/vnpt/movie_booking_be/service/MovieService.java
@@ -10,6 +10,8 @@ import java.util.Set;
 public interface MovieService {
     List<MovieResponse> getAllMovies();
 
+    List<MovieResponse> getAllMoviesDelete();
+
     void createMovie(String title, Set<Integer> genreIds, String description, MultipartFile file, MultipartFile video, String director, String casts, String duration, float rating, String releaseDate, String endDate);
 
     Movie updateMovie(Integer id, String title, Set<Integer> genreIds, String description, MultipartFile file, MultipartFile video, String director, String casts, String duration, String rating, String releaseDate, String endDate, boolean isActive);
